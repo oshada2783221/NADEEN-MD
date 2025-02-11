6f78709dc5d431a95a968cc6ebcd4935fef40a4e
oshada2783221 <ososhada36@gmail.com>
2025-02-11 20:34:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //put your session id
+SESSION_ID: process.env.SESSION_ID || "SAQxlRga#CEQRYf19OhOZB82iuYCPBS5HLK1dhLM30GicSBaWcNA", //put your session id
 MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority",
 MODE: process.env.MODE || "private", //private | public | groups | inbox
-ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true", //true or false 
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false", //true or false 
 AUTO_TYPING: process.env.AUTO_TYPING || "false", //true or false
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", //true or false
 AUTO_VOICE: process.env.AUTO_VOICE || "true" //true or false
